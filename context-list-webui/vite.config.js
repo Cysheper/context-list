895181e59c25e76f2efc011c2b5428b4fb72f3dd
Cysheper <cysheper@outlook.com>
2026-09-21 13:12:37 +0800

    5

--- context-list-webui/vite.config.js
+++ context-list-webui/vite.config.js
@@ -6,10 +6,6 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     host: '0.0.0.0',
-    proxy: {
-      '^/(get_all_contexts|add_context|change_context|health)(?:\\?|$)': {
-        target: 'http://127.0.0.1:8080',
-      },
-    },
+
   },
 })
