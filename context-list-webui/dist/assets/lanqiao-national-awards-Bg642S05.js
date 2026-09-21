1ac113eeda1bd112986e6d1cf26df49cd97181e8
Cysheper <cysheper@outlook.com>
2026-09-21 12:39:24 +0800

    2

---
+++ context-list-webui/dist/assets/lanqiao-national-awards-Bg642S05.js
@@ -0,0 +1,9 @@
+var e=`6月6日，第十七届蓝桥杯全国大学生软件和信息技术大赛（软件类）全国总决赛在南昌落下落幕，东华理工大学学子获得国家一等奖5项、二等奖11项、三等奖19项。
+
+![图片](https://cynite.oss-cn-guangzhou.aliyuncs.com/uploads/mmexport1789965242420.jpg)
+
+![合照](https://cynite.oss-cn-guangzhou.aliyuncs.com/uploads/mmexport1789965246773.jpg)
+
+据悉，蓝桥杯全国大学生软件和信息技术大赛是由中国软件行业协会举办的全国性比赛。大赛以“竞赛联动教学改革、实战赋能能力提升、创新驱动产学协同”为宗旨，致力于为高校软件和信息技术人才培养提供支撑，为学生搭建提升实战能力、对接产业需求的成长平台。该赛事已连续举办17届，参赛高校超过2100所，总参赛人数突破150万。
+
+> 来源![来源](https://mp.weixin.qq.com/s/bE03g31-pw1dtYlYqbrESA)`;export{e as default};
